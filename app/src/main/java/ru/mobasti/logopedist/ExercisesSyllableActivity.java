31a692ea90a6b64670dcc956086d96bd67a23542
Admin <lexanuch25@gmail.com>
2018-05-21 14:15:04 +0300

    18-05-2018

--- app/src/main/java/ru/mobasti/logopedist/ExercisesSyllableActivity.java
+++ app/src/main/java/ru/mobasti/logopedist/ExercisesSyllableActivity.java
@@ -2,9 +2,14 @@ package ru.mobasti.logopedist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class ExercisesSyllableActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,17 @@ public class ExercisesSyllableActivity extends Activity {
         Intent intent = getIntent();
 
         String letter = intent.getStringExtra("Letter");
+        TextView NameSyllable = (TextView)findViewById(R.id.textView_NameSyllable);
+        NameSyllable.setText(letter);
+
+        VideoView VideoSyllable = (VideoView)findViewById(R.id.videoView_Syllable);
 
 
 
+    }
+
+    public void VideoPlay (View v) {
+        String vidoepath = "android.resourse://ru.mobasti.logopedist/" + R.raw.test_video.mp4;
+        Url uri = Uri.parse()
     }
 }
